ae8a0c6ddec75b7dc6764f2c51246fa3f6e50292
Petr Menšík <pemensik@redhat.com>
2024-08-23 12:31:43 +0200

    Get list of active branches from bodhi
    Use separate fetch from bodhi API to get list of pending and current
    branches. Test each of those has rawhide correctly updated in spec file.
    
    Skip eln branch in any case.

--- tests/branches-rawhide-release/test.sh
+++ tests/branches-rawhide-release/test.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
-# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k expandtab sts=4
 #
 # Inspired by distribution-gpg-keys github workflow
 # https://github.com/rpm-software-management/distribution-gpg-keys/blob/main/.github/workflows/fedora-repos.yml
+# Bodhi fetching of active branches inspired from github action
+# https://github.com/sgallagher/get-fedora-releases-action/blob/main/get_fedora_releases.py
 #
 # Checks whether active releases point to correct rawhide version
 . /usr/share/beakerlib/beakerlib.sh || exit 1
 
 
-REPOURL=https://src.fedoraproject.org/rpms/fedora-repos.git
-CLONEDIR=fedora-repos
+: ${REPOURL:=https://src.fedoraproject.org/rpms/fedora-repos.git}
+: ${BODHI_RELEASES_URL:="https://bodhi.fedoraproject.org/releases/"}
+: ${CLONEDIR:=fedora-repos}
 SPECPATH="${CLONEDIR}.spec"
 
 spec_rawhide_release() {
-	local SPEC=$1
-	awk '$1 == "%global" && $2 == "rawhide_release" { print $3 } ' $SPEC
+    local SPEC=$1
+    awk '$1 == "%global" && $2 == "rawhide_release" { print $3 } ' $SPEC
+}
+
+get_bodhi_branches() {
+    local FILE="$1"
+    shift
+    curl -fL "${BODHI_CURRENT_URL}$*" -o "$FILE"
+}
+
+parse_bodhi_branches() {
+    jq -r '.releases[] | select(.id_prefix == "FEDORA") | .branch' "$@" | xargs echo
 }
 
 rlJournalStart
@@ -22,21 +35,32 @@ rlJournalStart
         rlRun "tmp=\$(mktemp -d)" 0 "Create tmp directory"
         rlRun "pushd $tmp"
         rlRun "git clone $REPOURL --depth 1 --no-single-branch $CLONEDIR"
+        rlRun "curl -fL \"${BODHI_RELEASES_URL}?state=current\" -o releases.current"
+        rlRun "curl -fL \"${BODHI_RELEASES_URL}?state=pending\" -o releases.pending"
+        rlRun "BODHI_STABLE_BRANCHES=\"$(parse_bodhi_branches releases.current)\"" 0 "Obtain supported stable branches from bodhi"
+        rlRun "BODHI_PENDING_BRANCHES=\"$(parse_bodhi_branches releases.pending)\"" 0 "Obtain pending branches from bodhi"
     rlPhaseEnd
 
     rlPhaseStartTest
         pushd $CLONEDIR
         rlRun "RAWHIDE_RELEASE=$(spec_rawhide_release $SPECPATH)"
+        rlAssertGreater "Check we have positive number" "$RAWHIDE_RELEASE" 0
+        # If branching is still in development
         for ((R=RAWHIDE_RELEASE-1; R>=RAWHIDE_RELEASE-2; R--)); do
-            rlRun "git checkout f${R}"
+            rlRun "git checkout f${R}" 0 "Test f${R} branch"
             rlRun "RAWHIDE_BRANCH=$(spec_rawhide_release $SPECPATH)"
+            rlAssertGreater "Check we have positive number" "$RAWHIDE_BRANCH" 0
             rlAssertEquals "Check rawhide version is correct on f$R" "$RAWHIDE_RELEASE" "$RAWHIDE_BRANCH"
         done
-        # Just informative check for maybe finalized branch
-        # TODO: find reliable way to know, when branch is not supported anymore
-        R=((RAWHIDE_RELEASE-3))
-        rlRun "git checkout f${R}"
-        rlRun "RAWHIDE_BRANCH=$(spec_rawhide_release $SPECPATH)"
+        for BRANCH in ${BODHI_STABLE_BRANCHES} ${BODHI_PENDING_BRANCHES}; do
+            if [ "$BRANCH" = eln ]; then
+                continue
+            fi
+            rlRun "git checkout $BRANCH" 0 "Test $BRANCH bodhi branch"
+            rlRun "RAWHIDE_BODHI_BRANCH=$(spec_rawhide_release $SPECPATH)"
+            rlAssertGreater "Check we have positive number" "$RAWHIDE_BODHI_BRANCH" 0
+            rlAssertEquals "Check rawhide version is correct on bodhi $BRANCH" "$RAWHIDE_RELEASE" "$RAWHIDE_BODHI_BRANCH"
+        done
         popd
     rlPhaseEnd
 
